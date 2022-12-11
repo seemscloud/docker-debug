0f2573ce9245b8f379fb554b5866d8f4c22c4410
Mateusz Katana <mateusz.katana@lppsa.com>
2022-12-11 01:09:50 +0100

    fds

--- mgmt.sh
+++ mgmt.sh
@@ -6,7 +6,7 @@ BUILD_DIR="docker"
 PROJECT_PATH="${PWD_PATH}/${SCRIPT_DIR}"
 
 DOCKER_REGISTRY="seemscloud"
-DOCKER_REPO="network-debugging"
+DOCKER_REPO="debug"
 DOCKER_TAG="latest"
 
 REPOSITORY="${DOCKER_REGISTRY}/${DOCKER_REPO}:${DOCKER_TAG}"
@@ -14,31 +14,33 @@ REPOSITORY="${DOCKER_REGISTRY}/${DOCKER_REPO}:${DOCKER_TAG}"
 function mgmt() {
   case "${1}" in
   build)
-    docker build \
-      --rm --force-rm --pull --no-cache \
-      --tag "${REPOSITORY}" \
-      "${PROJECT_PATH}/${BUILD_DIR}"
+    docker build --rm --force-rm --pull --no-cache --tag "${REPOSITORY}" "${PROJECT_PATH}/${BUILD_DIR}"
     ;;
-  push)
-    docker push "${REPOSITORY}"
+  cache)
+    docker build --rm --force-rm --pull --tag "${REPOSITORY}" "${PROJECT_PATH}/${BUILD_DIR}"
     ;;
-  run)
-    docker run \
-      --detach \
-      --interactive --rm \
-      --name "${DOCKER_REPO}" \
-      "${REPOSITORY}"
+  test)
+    mgmt rm container
+    docker run --detach --interactive --rm --name "${DOCKER_REPO}" "${REPOSITORY}"
+    docker exec -it ${DOCKER_REPO} bash
     ;;
   rm)
-    docker container rm \
-      --force \
-      "${DOCKER_REPO}"
+    mgmt rm-container
+    docker image rm "${REPOSITORY}"
     ;;
-  sh)
-    docker exec -it \
-      ${DOCKER_REPO} sh
+  rm-container)
+    docker container rm --force "${DOCKER_REPO}"
+    ;;
+  push)
+    mgmt build
+    docker push "${REPOSITORY}"
+    ;;
+  prune)
+    docker container prune -f
+    docker image prune -f
+    docker system prune -f
     ;;
   esac
 }
 
-mgmt "${1}"
+mgmt "${1}" "${2}"
